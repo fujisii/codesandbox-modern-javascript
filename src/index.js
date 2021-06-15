ee07df364ea872548b103c9a839d63c4a0445a82
fujisii <51264835+fujisii@users.noreply.github.com>
2021-06-15 22:43:57 +0900

    24. mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -132,15 +132,15 @@
 // console.log(num2);
 // console.log(arr3);
 
-// 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// // 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-// 配列をコピーするときはスプレッド構文を使用する
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr6);
-console.log(arr4);
+// // 配列をコピーするときはスプレッド構文を使用する
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
@@ -149,3 +149,34 @@ console.log(arr4);
 // const arr8 = arr4;
 // arr8[0] = 100;
 // console.log(arr4); // [10, 20]ではなく[100, 20]で出力される
+
+/**
+ * 24. mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(`${name}です`));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
