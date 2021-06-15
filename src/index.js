156d4d1133df40506685ee2be778a031f38cec3a
fujisii <51264835+fujisii@users.noreply.github.com>
2021-06-15 22:14:35 +0900

    23. スプレッド構文 ...

--- src/index.js
+++ src/index.js
@@ -110,5 +110,42 @@
 /**
  * デフォルト値、引数など
  */
-const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
-sayHello("じゃけぇ");
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("じゃけぇ");
+
+/**
+ * スプレッド構文　 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+// 配列をコピーするときはスプレッド構文を使用する
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// イコールで配列を渡してしまうとコピーではなく参照渡しになってしまうので注意
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4); // [10, 20]ではなく[100, 20]で出力される
