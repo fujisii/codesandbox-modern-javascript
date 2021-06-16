6bfefef9b40ddd5b02f4348443aacddfc825f54f
fujisii <51264835+fujisii@users.noreply.github.com>
2021-06-16 22:30:15 +0900

    25. 三項演算子 ? :

--- src/index.js
+++ src/index.js
@@ -180,3 +180,24 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueのとき : 条件がfalseのとき
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// // toLocaleString()とは
+// // 数値を指定したロケールとオプションに応じた形式で文字列に変換した値を取得することができる
+// // console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+};
+console.log(checkSum(50, 40));
